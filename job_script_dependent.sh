ed2bffa53998df215e1c2b93c7ac426c9f5fe089
FredericOuimetMcGill <frederic.ouimet2@uqtr.ca>
2025-10-28 13:12:42 -0400

    Add files via upload

---
+++ job_script_dependent.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+#SBATCH --time=72:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=63
+#SBATCH --mem=240G
+#SBATCH -o log/%x_%j.out
+#SBATCH --mail-user=frederic.ouimet.23@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=message/%x_%j-result.txt
+#SBATCH --error=message/%x_%j-error.txt
+#SBATCH --exclusive
+#SBATCH --account=def-cgenest
+
+module load StdEnv/2023 r/4.3.1
+Rscript Wishart_density_estimation_dependent.R
